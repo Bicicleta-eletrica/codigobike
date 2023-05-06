b891b1df7e517e45c4dd5f841d5cf4a3468336de
GilmarGiroto <128000428+GilmarGiroto@users.noreply.github.com>
2023-05-05 22:24:47 -0300

    codigo_login

---
+++ cod_bike.py
@@ -0,0 +1,44 @@
+usuarios=[]
+senhas=[]
+
+from getpass import getpass
+from hashlib import sha256
+
+while True:
+    opcao=input("Pressione 1 para acessar sua conta, 2 para criar uma conta ou 3 para sair: ")
+
+    if int(opcao) == 1:
+        usuario=input("Insira o usuário: ")
+        senha=getpass(print("Insira sua senha: "))
+        senha=sha256(senha.encode()).digest()
+        if usuario in usuarios and senha in senhas:
+            print("Conectado com sucesso!")
+        else:
+            print("Usuário ou senha inválidos, tente novamente.")
+    
+
+    
+    elif int(opcao) == 2:
+        print("Cadastre seu usuário e senha")
+        
+        usuario=str(input("Insira seu usuário: "))
+        if usuario in usuarios:
+            print("Usuario existente, tente outro!")
+        else:
+            usuarios.append(usuario)
+        
+            senha=getpass(print("Insira sua senha: "))
+            senha=sha256(senha.encode()).digest()
+            senhas.append(senha)
+            print("Conta criada com sucesso!")
+
+
+    elif int(opcao) == 3:
+        print("Obrigado por acessar!")
+        break
+    
+
+    else: 
+        print("Opção Inválida, tente novamente")
+
+ 
